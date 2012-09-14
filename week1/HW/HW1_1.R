e5105f6436f5e33327899fe615a845518953c18c
Sau <saurabhmadaan@gmail.com>
2012-09-14 08:36:24 -0700

    adding hw pdfs latex

--- week1/HW/HW1_1.R
+++ week1/HW/HW1_1.R
@@ -1,12 +1,24 @@
-setwd("/Users/smadaan/Documents/ML_UCSC/week1/HW")
+##--Q1s----
+setwd("/Users/Saurabh/Documents/ML_UCSC/week1/HW")
 data<-read.csv("HW01pb1data.csv",header=FALSE)
 class(data)
-str(data)
+str(data) 
+#800 obs. of 5 variables. V1-3 are int, V4-5 are Factors
+##--Q1e----
 
-is.factor(data[,4])
-as.numeric(data[,5])
+##--Q2s----
+l4<-levels(data[,4]) 
+#has integers, and "thirty five"
+which(data[,4]=="thirty five")
+#[1] 405
 
-l4<-levels(data[,4])
+l5<-levels(data[,5]) 
+#has integers, and "twenty five"
+which(data[,5]=="twenty five")
+# [1] 531
+##--Q2e----
+
+
 #l4
 '
 > l4
@@ -18,11 +30,6 @@ l4<-levels(data[,4])
 '
 
 
-which(data[,4]=="thirty five")
-#[1] 405
-
-l5<-levels(data[,5])
-#l5
 '
 > l5
  [1] "0"           "10"          "120"         "140"         "15"         
@@ -30,10 +37,13 @@ l5<-levels(data[,5])
 [11] "40"          "45"          "5"           "50"          "55"         
 [16] "60"          "70"          "80"          "twenty five"
 '
-# [1] 531
 
+##--Q31s----
 plot(data[,1])
+##--Q31e----
+##--Q32s----
 plot(data[,4])
+##--Q32e----
 
 
 
--- week1/HW/HW1_2.R
+++ week1/HW/HW1_2.R
@@ -1,48 +1,35 @@
-setwd("/Users/smadaan/Documents/ML_UCSC/week1/HW")
+##--Q1s----
+setwd("/Users/Saurabh/Documents/ML_UCSC/week1/HW")
 data<-read.csv("HW01pb2data.csv",header=FALSE)
-class(data)
 str(data)
 # 'data.frame':	2000000 obs. of  1 variable:
-
-is.factor(data[,1])
-#FALSE
-is.numeric(data[,1])
-#TRUE
-
-plot(data[,1])
-plot(data[,4])
-
-length(data[,1])
-#[1] 2000000
 
 nrow(data)
 #[1] 2000000
 
 # selecting a sample of 10,000 random records
-ss<-seq(1,2000000)
-rand.ind<-sample(ss,10000,replace=F)
+ss<-seq(1,nrow(data))
+rand.ind<-sample(ss,10000,replace=F) #set of random indices for subset
 small_data<-data[,1][rand.ind]
-small_data
-length(small_data)
+length(small_data) 
+#[1] 10000
+##--Q1e----
 
+
+##--Q2s----
 #mean, max and other descriptive stats
-mean(small_data)
-max(small_data)
-var(small_data)
-quantile(small_data,0.25)
-'
-> length(small_data)
-[1] 10000
-> mean(small_data)
-[1] 9.41002
-> max(small_data)
-[1] 16.93748
-> var(small_data)
-[1] 4.004991
-> quantile(small_data,0.25)
-     25% 
-8.079612 
-'
+mean(small_data) 
+#[1] 9.41002
+max(small_data) 
+#[1] 16.93748
+var(small_data) 
+#[1] 4.004991
+quantile(small_data,0.25) 
+#8.079612
+##--Q2e----
+
+
+
 mean(data)
 max(data)
 var(data)
